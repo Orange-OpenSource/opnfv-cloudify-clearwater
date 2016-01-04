0d87f09f84e0b1b7d602a2c9f7f4070f094ae709
boucherv-orange <valentin.boucher@orange.com>
2016-01-04 13:45:04 +0100

    docs 3.2 -> 3.3

--- scripts/clearwater/ellis/install-ellis.sh
+++ scripts/clearwater/ellis/install-ellis.sh
@@ -9,7 +9,7 @@ sudo apt-get update
 
 ctx logger info "Installing ellis packages and other clearwater packages"
 sudo  DEBIAN_FRONTEND=noninteractive apt-get install ellis --yes --force-yes -o DPkg::options::=--force-confnew
-sudo  DEBIAN_FRONTEND=noninteractive apt-get install clearwater-config-manager --yes --force-yes
+sudo DEBIAN_FRONTEND=noninteractive apt-get install clearwater-management --yes --force-yes
 ctx logger info "The installation packages is done correctly"
 
 ctx logger info "Configure a new DNS server"
