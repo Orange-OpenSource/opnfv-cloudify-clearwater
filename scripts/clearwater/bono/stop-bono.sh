81348cbf58c50a84d876d41f689f6a6b267639e2
boucherv-orange <valentin.boucher@orange.com>
2016-01-07 13:28:12 +0100

    set timeout for bono

--- scripts/clearwater/bono/stop-bono.sh
+++ scripts/clearwater/bono/stop-bono.sh
@@ -4,7 +4,7 @@ ctx logger info "Stopping bono node"
 
 sudo monit unmonitor -g bono
 
-sudo service bono quiesce
+sudo timeout 180 service bono quiesce
 
 sudo monit unmonitor clearwater_config_manager
 sudo monit unmonitor -g etcd
