b08180803c6a211a00048fa5afc530fce625bbae
Baneamoon <asrabaneamoon@yahoo.com>
2016-09-21 10:16:20 +0200

    Update install-sprout.sh

--- scripts/clearwater/sprout/install-sprout.sh
+++ scripts/clearwater/sprout/install-sprout.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+!/bin/bash -e
 
 ctx logger debug "${COMMAND}"
 
@@ -6,17 +6,14 @@ sudo mkdir -p /etc/chronos
 
 echo '
 [http]
-bind-address = $(hostname -I)
+bind-address = 10.0.0.4
 bind-port = 7253
 threads = 50
-
 [logging]
 folder = /var/log/chronos
 level = 2
-
 [alarms]
 enabled = true
-
 [exceptions]
 max_ttl = 600' | sudo tee --append /etc/chronos/chronos.conf
 
@@ -33,8 +30,16 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install sprout --yes --force-yes -o 
 sudo DEBIAN_FRONTEND=noninteractive  apt-get install clearwater-management --yes --force-yes
 ctx logger info "The installation packages is done correctly"
 
+
+echo '
+log_level=5
+
+authentication='Y''| sudo tee --append /etc/clearwater/user_settings
+
+
 ctx logger info "Use the DNS server"
 echo 'RESOLV_CONF=/etc/dnsmasq.resolv.conf' | sudo tee --append  /etc/default/dnsmasq
 sudo service dnsmasq force-reload
 
 ctx logger info "Installation is done"
+echo "END "
