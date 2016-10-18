554f1b55b58ff1a418c41f401c4957338fffe5c6
Baneamoon <asrabaneamoon@yahoo.com>
2016-10-18 18:17:53 +0200

    Update install-ralf.sh

--- scripts/clearwater/ralf/install-ralf.sh
+++ scripts/clearwater/ralf/install-ralf.sh
@@ -29,7 +29,7 @@ fi
 sudo apt-get update
 
 ctx logger info "Installing ralf packages and other clearwater packages"
-sudo DEBIAN_FRONTEND=noninteractive apt-get install ralf --yes --force-yes -o DPkg::options::=--force-confnew
+sudo DEBIAN_FRONTEND=noninteractive apt-get install ralf-node --yes --force-yes -o DPkg::options::=--force-confnew
 sudo DEBIAN_FRONTEND=noninteractive  apt-get install clearwater-management --yes --force-yes
 ctx logger info "The installation packages is done correctly"
 
