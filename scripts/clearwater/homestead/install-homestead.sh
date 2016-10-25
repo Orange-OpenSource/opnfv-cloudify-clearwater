1df85c5437da70556a7bd902e657e6b54530affd
Valentin Boucher <valentin.boucher@orange.com>
2016-10-25 13:01:28 +0200

    Update install-homestead.sh

--- scripts/clearwater/homestead/install-homestead.sh
+++ scripts/clearwater/homestead/install-homestead.sh
@@ -12,10 +12,9 @@ sudo apt-get update
 
 ctx logger info "Installing homestead packages and other clearwater packages"
 set +e
-sudo DEBIAN_FRONTEND=noninteractive  apt-get -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew install homestead homestead-prov clearwater-prov-tools --yes --force-yes
+sudo DEBIAN_FRONTEND=noninteractive  apt-get -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew install homestead-node clearwater-prov-tools --yes --force-yes
 sudo DEBIAN_FRONTEND=noninteractive  apt-get install clearwater-management --yes --force-yes
 set -e
-sudo sed -i '47d' /etc/monit/conf.d/clearwater-cassandra.monit 
 ctx logger info "The installation packages is done correctly"
 
 ctx logger info "Use the DNS server"
