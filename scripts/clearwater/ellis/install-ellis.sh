55626912aef56e58cad5e13e11f66454d3331c26
Baneamoon <asrabaneamoon@yahoo.com>
2016-12-26 17:13:56 +0100

    Update install-ellis.sh

--- scripts/clearwater/ellis/install-ellis.sh
+++ scripts/clearwater/ellis/install-ellis.sh
@@ -11,8 +11,8 @@ fi
 sudo apt-get update
 
 ctx logger info "Installing ellis packages and other clearwater packages"
-sudo  DEBIAN_FRONTEND=noninteractive apt-get install ellis-node --yes --force-yes -o DPkg::options::=--force-confnew
-sudo DEBIAN_FRONTEND=noninteractive apt-get install clearwater-management --yes --force-yes
+sudo DEBIAN_FRONTEND=noninteractive apt-get install ellis-node --yes
+sudo DEBIAN_FRONTEND=noninteractive apt-get install clearwater-management --yes
 ctx logger info "The installation packages is done correctly"
 
 ctx logger info "Configure a new DNS server"
