b066da929b5ed01fef4ed7319edabb040d09c7b2
boucherv <valentin.boucher@orange.com>
2017-02-15 16:42:00 +0100

    add release param

--- scripts/clearwater/homer/install-homer.sh
+++ scripts/clearwater/homer/install-homer.sh
@@ -2,10 +2,17 @@
 
 ctx logger debug "${COMMAND}"
 
+release=$(ctx node properties release)
+
 ctx logger info "Configure the APT software source"
 if [ ! -f /etc/apt/sources.list.d/clearwater.list ]
   then
-    echo 'deb http://repo.cw-ngv.com/stable binary/' | sudo tee --append /etc/apt/sources.list.d/clearwater.list
+    if [ $release = "stable" ]
+    then
+      echo 'deb http://repo.cw-ngv.com/stable binary/' | sudo tee --append /etc/apt/sources.list.d/clearwater.list
+    else
+      echo "deb http://repo.cw-ngv.com/archive/$release binary/" | sudo tee --append /etc/apt/sources.list.d/clearwater.list
+    fi
     curl -L http://repo.cw-ngv.com/repo_key | sudo apt-key add -
 fi
 sudo apt-get update
@@ -15,7 +22,6 @@ set +e
 sudo DEBIAN_FRONTEND=noninteractive  apt-get -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew install homer --yes --force-yes
 sudo DEBIAN_FRONTEND=noninteractive  apt-get install clearwater-management --yes --force-yes
 set -e
-sudo sed -i '47d' /etc/monit/conf.d/clearwater-cassandra.monit
 ctx logger info "The installation packages is done correctly"
 
 ctx logger info "Use the DNS server"
