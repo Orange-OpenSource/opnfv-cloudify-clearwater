9ddc70a4e21340c8d326a03053cc2b8143511d56
boucherv <valentin.boucher@orange.com>
2017-03-20 10:28:17 +0100

    update package name for ellis

--- scripts/clearwater/other/install-etcd.sh
+++ scripts/clearwater/other/install-etcd.sh
@@ -3,15 +3,18 @@
 ctx logger debug "${COMMAND}"
 
 ctx logger info "Configure the APT software source"
-echo 'deb http://repo.cw-ngv.com/archive/repo107 binary/' | sudo tee --append /etc/apt/sources.list.d/clearwater.list
-curl -L http://repo.cw-ngv.com/repo_key | sudo apt-key add -
+if [ ! -f /etc/apt/sources.list.d/clearwater.list ]
+  then
+    echo 'deb http://repo.cw-ngv.com/archive/repo107 binary/' | sudo tee --append /etc/apt/sources.list.d/clearwater.list
+    curl -L http://repo.cw-ngv.com/repo_key | sudo apt-key add -
+fi
 sudo apt-get update
 
 ctx logger info "Now install the software"
-sudo   DEBIAN_FRONTEND=noninteractive apt-get install clearwater-config-manager --yes --force-yes
+sudo DEBIAN_FRONTEND=noninteractive apt-get install clearwater-management --yes --force-yes
 ctx logger info "The software is installed"
 
+/usr/share/clearwater/clearwater-etcd/scripts/wait_for_etcd
 sudo /usr/share/clearwater/clearwater-config-manager/scripts/upload_shared_config
-#sudo /usr/share/clearwater/clearwater-config-manager/scripts/apply_shared_config
 
 ctx logger info "Installation is done"
