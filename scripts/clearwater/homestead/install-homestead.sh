2f3090bf44cdebc1024402770681b8833f5ad448
boucherv <valentin.boucher@orange.com>
2017-04-03 15:48:56 +0200

    fix repo107

--- scripts/clearwater/homestead/install-homestead.sh
+++ scripts/clearwater/homestead/install-homestead.sh
@@ -5,7 +5,7 @@ ctx logger debug "${COMMAND}"
 ctx logger info "Configure the APT software source"
 if [ ! -f /etc/apt/sources.list.d/clearwater.list ]
   then
-    echo 'deb http://repo.cw-ngv.com/stable binary/' | sudo tee --append /etc/apt/sources.list.d/clearwater.list
+     echo 'deb http://repo.cw-ngv.com/archive/repo107 binary/' | sudo tee --append /etc/apt/sources.list.d/clearwater.list
     curl -L http://repo.cw-ngv.com/repo_key | sudo apt-key add -
 fi
 sudo apt-get update
@@ -15,7 +15,7 @@ set +e
 sudo DEBIAN_FRONTEND=noninteractive  apt-get -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew install homestead homestead-prov clearwater-prov-tools --yes --force-yes
 sudo DEBIAN_FRONTEND=noninteractive  apt-get install clearwater-management --yes --force-yes
 set -e
-sudo sed -i '47d' /etc/monit/conf.d/clearwater-cassandra.monit 
+sudo sed -i '47d' /etc/monit/conf.d/clearwater-cassandra.monit
 ctx logger info "The installation packages is done correctly"
 
 ctx logger info "Use the DNS server"
--- scripts/clearwater/other/install-etcd.sh
+++ scripts/clearwater/other/install-etcd.sh
@@ -3,7 +3,7 @@
 
 if [ ! -f /etc/apt/sources.list.d/clearwater.list ]
   then
-    echo 'deb http://repo.cw-ngv.com/stable binary/' | sudo tee --append /etc/apt/sources.list.d/clearwater.list
+     echo 'deb http://repo.cw-ngv.com/archive/repo107 binary/' | sudo tee --append /etc/apt/sources.list.d/clearwater.list
     curl -L http://repo.cw-ngv.com/repo_key | sudo apt-key add -
 fi
 sudo apt-get update
@@ -12,4 +12,3 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install clearwater-management --yes 
 
 #/usr/share/clearwater/clearwater-etcd/scripts/wait_for_etcd
 #sudo /usr/share/clearwater/clearwater-config-manager/scripts/upload_shared_config
-
