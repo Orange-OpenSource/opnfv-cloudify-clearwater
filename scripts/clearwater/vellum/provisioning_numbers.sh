532b820f33d1b4856d1d706c7e18fd3f0aeed684
Valentin Boucher <valentin.boucher@orange.com>
2017-08-05 11:28:54 +0200

    Update provisioning_numbers.sh

--- scripts/clearwater/vellum/provisioning_numbers.sh
+++ scripts/clearwater/vellum/provisioning_numbers.sh
@@ -11,17 +11,19 @@ do
   sleep 10
 done
 
-if [ ! -d "/usr/share/clearwater/crest/tools/sstable_provisioning/homestead_cache" ]; then
-  cd /usr/share/clearwater/crest/tools/sstable_provisioning/
+cd /usr/share/clearwater/crest/tools/sstable_provisioning/
 
+if [ ! -d "/usr/share/clearwater/crest/tools/sstable_provisioning/homestead_cache" ]; then
   sudo ./BulkProvision homestead-local $start_number $end_number $public_domain toto
   sudo ./BulkProvision homestead-hss $start_number $end_number $public_domain toto
   sudo ./BulkProvision homer $start_number $end_number $public_domain toto
-  sstableloader -v -d ${cassandra_hostname:-$local_ip} homer/simservs
-  sstableloader -v -d ${cassandra_hostname:-$local_ip} homestead_cache/impi
-  sstableloader -v -d ${cassandra_hostname:-$local_ip} homestead_cache/impu
-  sstableloader -v -d ${cassandra_hostname:-$local_ip} homestead_provisioning/implicit_registration_sets
-  sstableloader -v -d ${cassandra_hostname:-$local_ip} homestead_provisioning/public
-  sstableloader -v -d ${cassandra_hostname:-$local_ip} homestead_provisioning/private
-  sstableloader -v -d ${cassandra_hostname:-$local_ip} homestead_provisioning/service_profiles
 fi
+sleep 30
+
+sstableloader -v -d ${cassandra_hostname:-$local_ip} homer/simservs
+sstableloader -v -d ${cassandra_hostname:-$local_ip} homestead_cache/impi
+sstableloader -v -d ${cassandra_hostname:-$local_ip} homestead_cache/impu
+sstableloader -v -d ${cassandra_hostname:-$local_ip} homestead_provisioning/implicit_registration_sets
+sstableloader -v -d ${cassandra_hostname:-$local_ip} homestead_provisioning/public
+sstableloader -v -d ${cassandra_hostname:-$local_ip} homestead_provisioning/private
+sstableloader -v -d ${cassandra_hostname:-$local_ip} homestead_provisioning/service_profiles
