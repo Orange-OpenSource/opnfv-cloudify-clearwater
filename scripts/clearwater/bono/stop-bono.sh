b42f16e8a879d15dff985327b98623aec7bb4895
Junaid Ali <junaidali.yahya@gmail.com>
2018-09-10 14:12:55 +0100

    Remove a Bono node from cluster
    Updating stop.sh script as per the instructions available at:
    https://clearwater.readthedocs.io/en/latest/Clearwater_Elastic_Scaling.html#if-you-did-a-manual-install

--- scripts/clearwater/bono/stop-bono.sh
+++ scripts/clearwater/bono/stop-bono.sh
@@ -6,5 +6,8 @@ sudo monit unmonitor -g bono
 
 sudo timeout 180 service bono quiesce
 
-sudo monit unmonitor clearwater_config_manager
+sudo monit unmonitor -g clearwater_cluster_manager
+sudo monit unmonitor -g clearwater_config_manager
+sudo monit unmonitor -g clearwater_queue_manager
 sudo monit unmonitor -g etcd
+sudo service clearwater-etcd decommission
