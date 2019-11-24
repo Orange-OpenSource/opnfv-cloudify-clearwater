ba281959090a643ce30e2f151b7ceac473dbbce8
Cédric Ollivier <cedric.ollivier@orange.com>
2019-11-24 13:23:51 +0100

    Switch to OPNFV Clearwater repository
    Project Clearwater is backed by Metaswitch Networks [1].
    They will therefore not be reinstating the 14.04 Project Clearwater
    repo server.
    
    Then all binaries have been compiled and published on
    http://artifacts.opnfv.org/functest/clearwater/debian
    
    Then it updates sources.list.d/clearwater.list
    
    [1] https://www.projectclearwater.org/
    
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>
    (cherry picked from commit 7969f872e6e9b796c9aecaf16f1a54d6fafc7f4b)

--- scripts/clearwater/ellis/install-ellis.sh
+++ scripts/clearwater/ellis/install-ellis.sh
@@ -14,13 +14,7 @@ ctx logger info "[ellis] ${release}"
 ctx logger info "[ellis] Configure the APT software source"
 if [ ! -f /etc/apt/sources.list.d/clearwater.list ]
   then
-    if [ $release = "stable" ]
-    then
-      echo 'deb http://repo.cw-ngv.com/stable binary/' | sudo tee --append /etc/apt/sources.list.d/clearwater.list
-    else
-      echo "deb http://repo.cw-ngv.com/archive/$release binary/" | sudo tee --append /etc/apt/sources.list.d/clearwater.list
-    fi
-    curl -L http://repo.cw-ngv.com/repo_key | sudo apt-key add -
+    echo 'deb http://artifacts.opnfv.org/functest/clearwater/debian ./' | sudo tee --append /etc/apt/sources.list.d/clearwater.list
 fi
 log sudo apt-get update
 
--- scripts/clearwater/sprout/install-sprout.sh
+++ scripts/clearwater/sprout/install-sprout.sh
@@ -14,13 +14,7 @@ ctx logger info "[sprout] ${release}"
 ctx logger info "[sprout] Configure the APT software source"
 if [ ! -f /etc/apt/sources.list.d/clearwater.list ]
   then
-    if [ $release = "stable" ]
-    then
-      echo 'deb http://repo.cw-ngv.com/stable binary/' | sudo tee --append /etc/apt/sources.list.d/clearwater.list
-    else
-      echo "deb http://repo.cw-ngv.com/archive/$release binary/" | sudo tee --append /etc/apt/sources.list.d/clearwater.list
-    fi
-    curl -L http://repo.cw-ngv.com/repo_key | sudo apt-key add -
+    echo 'deb http://artifacts.opnfv.org/functest/clearwater/debian ./' | sudo tee --append /etc/apt/sources.list.d/clearwater.list
 fi
 log sudo apt-get update
 
